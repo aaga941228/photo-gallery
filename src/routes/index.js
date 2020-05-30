a49a592e8311caab52a1ba063bc84ce2750051a4
Alan Ayala <aaga941228@outlook.es>
2020-05-29 20:58:41 -0500

    views

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,12 @@
 const { Router } = require("express");
 const router = Router();
 
-router.get("/", (req, res) => {
-  res.send("ok");
-});
+router
+  .get("/", (req, res) => {
+    res.render("image");
+  })
+  .get("/images/add", (req, res) => {
+    res.render("imageForm");
+  });
 
 module.exports = router;
